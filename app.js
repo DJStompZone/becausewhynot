9746dea04ffcd0ffc96a499fc3f613b8827c84bf
DJ Magar <85457381+DJStompZone@users.noreply.github.com>
2025-09-22 06:16:00 -0500

    Update dev server

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ * app.js — BWN Visualizer (Express dev server)
+ *
+ *
+ */
 
-var indexRouter = require('./routes/index');
-var victoryRouter = require('./routes/victory');
-var soundscapeRouter = require('./routes/soundscape');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var app = express();
+const indexRouter = require("./routes/index");
+const victoryRouter = require("./routes/victory");
+const soundscapeRouter = require("./routes/soundscape");
+const singularityRouter = require("./routes/singularity");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
-app.use(logger('dev'));
+// --- View engine setup ---
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// --- Middleware ---
+app.disable("x-powered-by");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/soundscape', soundscapeRouter);
-app.use('/victory', victoryRouter);
+// Serve static assets from /public at web root "/"
+app.use(express.static(path.join(__dirname, "public")));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// --- Routes ---
+app.use("/", indexRouter);
+app.use("/soundscape", soundscapeRouter);
+app.use("/victory", victoryRouter);
+app.use("/singularity", singularityRouter);
+
+app.get("/becausewhynot", (req, res) => res.render("index"));
+
+// --- 404 handler ---
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// --- Error handler ---
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
