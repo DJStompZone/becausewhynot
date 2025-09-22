73b7828f58b47bd18d88197b31dd31b37cd9d0ba
DJ Magar <85457381+DJStompZone@users.noreply.github.com>
2025-09-22 06:17:13 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Visual' });
+  res.render('index', { title: 'DJ Stomp | Because Why Not' });
 });
 
 module.exports = router;
