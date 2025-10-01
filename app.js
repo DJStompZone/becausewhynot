539ed2c1371f2ef1e537cd371da581e36d0a5da1
DJ Stomp <85457381+DJStompZone@users.noreply.github.com>
2025-10-01 09:50:11 -0500

    Add route handlers for goodluck

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const victoryRouter = require("./routes/victory");
 const soundscapeRouter = require("./routes/soundscape");
 const singularityRouter = require("./routes/singularity");
 const whiplashRouter = require("./routes/whiplash");
+const goodluckRouter = require("./routes/goodluck");
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use("/soundscape", soundscapeRouter);
 app.use("/victory", victoryRouter);
 app.use("/singularity", singularityRouter);
 app.use("/whiplash", whiplashRouter);
+app.use("/goodluck", goodluckRouter);
 
 app.get("/becausewhynot", (req, res) => res.render("index"));
 
